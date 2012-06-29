de1d91985ed1a3fefc0bd49321630dc59278851b
Christopher Jackson <himself@cj-jackson.com>
2012-06-29 17:07:35 +0100

    Improved coding effiency of 'slugify.go'!

--- slugify.go
+++ slugify.go
@@ -6,24 +6,18 @@ import(
 )
 
 func S(str string) string {
-	r, _ := regexp.Compile("^ +| +$")						// Trim
-	str = string(r.ReplaceAll([]byte(str), []byte("")))
-	str = strings.ToLower(str)
+	str = strings.ToLower(strings.Trim(str, " "))						// Trim than Lower-Case
 	
 	from := "aáäâeéëeiíiîoóöőôuúüűunç·/_,:;"
 	to   := "aaaaeeeeiiiiooooouuuuunc------"
 	i := 0
 	for _, v := range from {
-		str = strings.Replace(str, string(v), string(to[i]), -1)	// Remove accents
+		str = strings.Replace(str, string(v), string(to[i]), -1)		// Remove accents
 		i++
 	}
 	
-	r, _ = regexp.Compile("[^a-z0-9 -]")
-	str = string(r.ReplaceAll([]byte(str), []byte("")))		// Remove invalid chars
-	r, _ = regexp.Compile(" +") 
-	str = string(r.ReplaceAll([]byte(str), []byte("-")))	// Collapse whitespace and replace by -
-	r, _ = regexp.Compile("-+")
-	str = string(r.ReplaceAll([]byte(str), []byte("-")))	// Collapse dashes
+	str = regexp.MustCompile("[^a-z0-9-]").ReplaceAllString(str, "-")	// Remove invalid chars (spaces are invalid)
+	str = regexp.MustCompile("-+").ReplaceAllString(str, "-")			// Collapse dashes
 	
 	return str
 }
