3417621f4fb609f1db190cf14ca97acdd46d163a
sauerbraten <willing.alexander@googlemail.com>
2012-10-15 20:33:37 +0200

    changed map[string]string to map[rune]string, cleaned up slugify_test.go

--- slugify.go
+++ slugify.go
@@ -5,44 +5,44 @@ import (
 	"strings"
 )
 
-var replacements = map[string]string{
-	"á": "a",
-	"à": "a",
-	"ä": "a",
-	"â": "a",
-	"é": "e",
-	"è": "e",
-	"ë": "e",
-	"ê": "e",
-	"í": "i",
-	"ì": "i",
-	"i": "i",
-	"î": "i",
-	"ó": "o",
-	"ò": "o",
-	"ö": "o",
-	"ő": "o",
-	"ô": "o",
-	"ú": "u",
-	"ù": "u",
-	"ü": "u",
-	"ű": "u",
-	"ç": "c",
-	"·": "-",
-	"/": "-",
-	"_": "-",
-	",": "-",
-	":": "-",
-	";": "-",
+var replacements = map[rune]string{
+	'á': "a",
+	'à': "a",
+	'ä': "a",
+	'â': "a",
+	'é': "e",
+	'è': "e",
+	'ë': "e",
+	'ê': "e",
+	'í': "i",
+	'ì': "i",
+	'i': "i",
+	'î': "i",
+	'ó': "o",
+	'ò': "o",
+	'ö': "o",
+	'ő': "o",
+	'ô': "o",
+	'ú': "u",
+	'ù': "u",
+	'ü': "u",
+	'ű': "u",
+	'ç': "c",
+	'·': "-",
+	'/': "-",
+	'_': "-",
+	',': "-",
+	':': "-",
+	';': "-",
 }
 
 func S(str string) string {
-	str = strings.ToLower(strings.TrimSpace(str)) // Trim, then Lower-Case
+	str = strings.ToLower(strings.TrimSpace(str)) // Trim all whitespace, then Lower-Case
 	newstr := ""
 
 	// Iterate over string, for each character check if there is a replacement, if so, append it to newstr, if not, append original value to newstr
 	for _, char := range str {
-		if replacement, ok := replacements[string(char)]; ok {
+		if replacement, ok := replacements[char]; ok {
 			newstr += replacement
 		} else {
 			newstr += string(char)
