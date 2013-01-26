f1c4fc2f6085a7a72bdef5d52129413333073fd0
sauerbraten <willing.alexander@googlemail.com>
2013-01-26 20:09:16 +0100

    fixe previous commit

--- slugify_test.go
+++ slugify_test.go
@@ -9,13 +9,20 @@ func TestSlugify(t *testing.T) {
 	if S("Hello World") != "hello-world" {
 		t.Fail()
 	}
+
 	if S("Hello World69") != "hello-world69" {
 		t.Fail()
 	}
+
 	if S("Hello           World????????") != "hello-world-" {
 		t.Fail()
 	}
+
 	if S("aáäâeéëeiíiîoóöőôuúüűunç·/_,:;") != "aaaaeeeeiiiiooooouuuuunc-" {
+		t.Fail()
+	}
+
+	if S("adam's song") != "adams-song" {
 		t.Fail()
 	}
 }
